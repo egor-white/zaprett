4451d789133016f04b1e799ce433e2f498172b3f
egor-white <channela082@gmail.com>
2025-06-14 12:15:48 +0300

    move files

--- src/customize.sh
+++ src/customize.sh
@@ -10,16 +10,52 @@ ui_print "(!) To download app, use Telegram chat"
 ui_print "Module by: egorwhite, Huananzhi X99, not_malware"
 ui_print "App by: egorwhite"
 ui_print "####################"
+
 ui_print "Unpacking archive..."
 unzip -o "$ZIPFILE" -x 'META-INF/*' -d $MODPATH >&2
+
 ui_print "Creating zaprett directory..."
 mkdir /sdcard/zaprett; mkdir /sdcard/zaprett/lists; mkdir /sdcard/zaprett/bin; mkdir /sdcard/zaprett/strategies; touch /sdcard/zaprett/config
+
 ui_print "Filling configuration file..."
-echo autorestart=true > /sdcard/zaprett/config; echo activelists=/storage/emulated/0/zaprett/lists/list-youtube.txt,/storage/emulated/0/zaprett/lists/list-discord.txt >> /sdcard/zaprett/config; echo zaprettdir=/sdcard/zaprett >> /sdcard/zaprett/config; echo strategy="" >> /sdcard/zaprett/config
+echo autorestart=true > /sdcard/zaprett/config; echo activelists=/storage/emulated/0/zaprett/lists/list-youtube.txt >> /sdcard/zaprett/config; echo zaprettdir=/sdcard/zaprett >> /sdcard/zaprett/config; echo strategy="" >> /sdcard/zaprett/config
+
 ui_print "Copying lists and binaries to /sdcard/zaprett..."
 cp -r $MODPATH/system/etc/zaprett/. /sdcard/zaprett/
+
+ui_print "Copying files to /bin"
+arch=$(uname -m)
+case "$arch" in
+    "x86_64")
+        nfqws="nfqws_x86_64"
+        ;;
+    "i386"|"i686")
+        nfqws="nfqws_x86"
+        ;;
+    "armv7l"|"arm")
+        nfqws="nfqws_arm32"
+        ;;
+    "aarch64"|"armv8l")
+        nfqws="nfqws_arm64"
+        ;;
+    "mips")
+        nfqws="nfqws_mips"
+        ;;
+    "mipsel")
+        nfqws="nfqws_mipsel"
+        ;;
+    *)
+        ui_print "Unknown arch: $arch"
+        abort
+        ;;
+esac
+cp $nfqws /bin/nfqws
+
 ui_print "Setting permissions..."
 chmod 777 /sdcard/zaprett; chmod 777 $MODPATH/service.sh
+
 ui_print "Cleaning temp files..."
 rm -rf $MODPATH/system/etc/zaprett
+rm -rf $MODPATH/system/bin
+
 ui_print "Installation done. Telegram chat: https://t.me/zaprett_module"
