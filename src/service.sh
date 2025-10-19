71e918bd132a5416f6f032365ef3eb7f66f61c26
CherretGit <petlindmitriy9@gmail.com>
2025-10-19 14:46:19 +0700

    Revert "Update service.sh"
    This reverts commit 3a7c40f167f33047ce18cabe99d15161f9fd3205.

--- src/service.sh
+++ src/service.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
 while [ -z "$(getprop sys.boot_completed)" ]; do sleep 2; done
-su -c "zaprett start"
-while true; do
-    sleep 3600
-    if [ -f "/data/adb/modules/zaprett/autostart" ]; then
-        su -c "zaprett restart"
-    fi
-done
+if [ -f "/data/adb/modules/zaprett/autostart" ]; then
+  su -c "zaprett start"
+  while true; do
+      sleep 3600
+	  su -c "zaprett restart"
+  done
+fi
