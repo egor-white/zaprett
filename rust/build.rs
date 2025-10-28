3c0488f629888211f37df951ac6f55cded4c6f73
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-10-29 06:58:55 +0800

    working!

--- rust/build.rs
+++ rust/build.rs
@@ -2,18 +2,36 @@ use once_cell::sync::Lazy;
 use std::env;
 use std::path::{Path, PathBuf};
 
-static NFQ: Lazy<PathBuf> = Lazy::new(|| {
-    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    Path::new(&manifest_dir).join("libs/zapret/nfq")
-});
+macro_rules! rel_manifest_path {
+    ($name:ident, $path:expr) => {
+        static $name: Lazy<PathBuf> = Lazy::new(|| {
+            let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+            Path::new(&manifest_dir).join($path)
+        });
+    };
+}
+
+rel_manifest_path!(NFQ, "libs/zapret/nfq");
+rel_manifest_path!(NFQ_CRYPTO, "libs/zapret/nfq/crypto");
 
 fn main() {
     cc::Build::new()
-        .file(NFQ.join("nfqws.c"))
+        .files(glob::glob(&format!("{}/*.c", NFQ.display()))
+            .unwrap()
+            .filter_map(Result::ok))
+        .files(glob::glob(&format!("{}/*.c", NFQ_CRYPTO.display()))
+            .unwrap()
+            .filter_map(Result::ok))
         .include(&*NFQ)
+        .include(&*NFQ_CRYPTO)
         .flag("-w")
-        .flag("-Dmain=nfqws_main")
+        .define("main", "nfqws_main")
         .compile("libnfqws.a");
+
+    println!("cargo:rustc-link-lib=z");
+    println!("cargo:rustc-link-lib=netfilter_queue");
+    println!("cargo:rustc-link-lib=nfnetlink");
+    println!("cargo:rustc-link-lib=mnl");
 
     println!("cargo:rustc-link-lib=static=nfqws");
     println!("cargo:rerun-if-changed={}", NFQ.display());
