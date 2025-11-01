d63cb7c6e60e9d6d5fba74d1a2bec5142dc4a85c
CherretGit <petlindmitriy9@gmail.com>
2025-11-01 22:08:35 +0700

    add saving logs from nfqws to files

--- rust/crates/zaprett/src/daemon.rs
+++ rust/crates/zaprett/src/daemon.rs
@@ -1,13 +1,20 @@
 use crate::{MODULE_PATH, run_nfqws};
 use daemonize::Daemonize;
 use log::{error, info};
+use std::fs::File;
 
 pub async fn daemonize_nfqws(args: &str) {
     info!("Starting nfqws as a daemon");
+
+    let stdout = File::create(MODULE_PATH.join("tmp/nfqws.out")).unwrap();
+    let stderr = File::create(MODULE_PATH.join("tmp/nfqws.err")).unwrap();
+
     let daemonize = Daemonize::new()
         .pid_file(MODULE_PATH.join("tmp/pid.lock").as_path())
         .working_directory("/tmp")
         // .group("daemon")
+        .stdout(stdout)
+        .stderr(stderr)
         .privileged_action(|| "Executed before drop privileges");
 
     match daemonize.start() {
